99e0f0f5667a3bddb7179d103c1f6810f0cdfaf5
CL0UDH <jorgelph77@gmail.com>
2020-06-10 19:12:16 -0500

    Terminamos la parte de imagenes, cargando una imagen desde la galeria o camara

--- lib/widgets/avatar.dart
+++ lib/widgets/avatar.dart
@@ -1,9 +1,26 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
+import 'package:image_picker/image_picker.dart';
 import 'circle_container.dart';
 
-class Avatar extends StatelessWidget {
+class Avatar extends StatefulWidget {
+  @override
+  _AvatarState createState() => _AvatarState();
+}
+
+class _AvatarState extends State<Avatar> {
+  File _file;
+
+  _pickImage() async {
+    final File file = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _file = file;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -11,13 +28,30 @@ class Avatar extends StatelessWidget {
       height: 200,
       child: Stack(
         children: <Widget>[
-          SvgPicture.network(
-            'https://image.flaticon.com/icons/svg/1946/1946369.svg'
-          ),
+          _file == null
+              ? SvgPicture.network(
+                  'https://image.flaticon.com/icons/svg/1946/1946369.svg')
+              : ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.file(
+                    _file,
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
           Positioned(
-            child: CircleContainer(
-              child: Icon(Icons.edit, color: Colors.blue,),
-              size: 60,
+            child: CupertinoButton(
+              padding: EdgeInsets.zero,
+              minSize: 30,
+              onPressed: _pickImage,
+              child: CircleContainer(
+                child: Icon(
+                  Icons.edit,
+                  color: Colors.blue,
+                ),
+                size: 60,
+              ),
             ),
             right: 0,
           ),
